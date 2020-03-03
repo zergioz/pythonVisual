f18c44da1c16a537ec3f9a3f577f61b016190029
Sergio F. Rodriguez <serrod@gmail.com>
2020-03-03 15:44:34 -0500

    fixed variable naming

--- getTimeSeries.py
+++ getTimeSeries.py
@@ -6,7 +6,7 @@ from privateKey import privateKeyModule
 jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&datatype=json&apikey='+privateKeyModule.keyValue
 jsonResult = requests.get(jsonUrl).json()
 
-with open('file.json', 'w', encoding='utf-8') as f:
-    json.dump(jsonResult, f, ensure_ascii=False, indent=4)
+with open('file.json', 'w', encoding='utf-8') as jsonFile:
+    json.dump(jsonResult, jsonFile, ensure_ascii=False, indent=4)
 
 
