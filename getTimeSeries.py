5fe5cfc3c6b53290ead4ed26b1807af7e83708f1
Sergio F. Rodriguez <serrod@gmail.com>
2020-03-04 14:53:59 -0500

    break script into functions

--- getTimeSeries.py
+++ getTimeSeries.py
@@ -2,18 +2,20 @@ import requests
 import json
 from privateKey import privateKeyModule
 
-print("Provide ticker symbol:")
-tickerInput = str(input())
-
-
-def getJSON(tiker):
-    jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+tiker+'&datatype=json&apikey='+privateKeyModule.keyValue
+def getJSON(ticker):
+    jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&datatype=json&apikey='+privateKeyModule.keyValue
     jsonResult = requests.get(jsonUrl).json()
-    with open(tickerInput+'.json', 'w', encoding='utf-8') as jsonFile:
+    with open(ticker+'.json', 'w', encoding='utf-8') as jsonFile:
         json.dump(jsonResult, jsonFile, ensure_ascii=False, indent=4) 
 
-if tickerInput: 
-   getJSON(tickerInput)
+def getInput():   
+    print("Provide ticker symbol:")
+    tickerInput = str(input())
+    return tickerInput
+
+tickerInput = getInput()
+if tickerInput:
+    getJSON(tickerInput)
 else:
     print("Sorry, no ticker provided. Exiting now.")
     exit
