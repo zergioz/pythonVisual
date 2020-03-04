1b1d7717cd18ea888dfc002e821db24253fc6557
Sergio F. Rodriguez <serrod@gmail.com>
2020-03-04 14:26:35 -0500

    added getJSON function and validator

--- getTimeSeries.py
+++ getTimeSeries.py
@@ -2,12 +2,19 @@ import requests
 import json
 from privateKey import privateKeyModule
 
-print("provide TICKER Symbol to fetch")
-tickerInput = str(input()) 
-jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+tickerInput+'&datatype=json&apikey='+privateKeyModule.keyValue
-jsonResult = requests.get(jsonUrl).json()
+print("Provide ticker symbol:")
+tickerInput = str(input())
 
-with open(tickerInput+'.json', 'w', encoding='utf-8') as jsonFile:
-    json.dump(jsonResult, jsonFile, ensure_ascii=False, indent=4)
 
+def getJSON(tiker):
+    jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+tiker+'&datatype=json&apikey='+privateKeyModule.keyValue
+    jsonResult = requests.get(jsonUrl).json()
+    with open(tickerInput+'.json', 'w', encoding='utf-8') as jsonFile:
+        json.dump(jsonResult, jsonFile, ensure_ascii=False, indent=4) 
+
+if tickerInput: 
+   getJSON(tickerInput)
+else:
+    print("Sorry, no ticker provided. Exiting now.")
+    exit
 
