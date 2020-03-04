77eda1a07d27dc5fe8b7c441c6c590e6b891670d
Sergio F. Rodriguez <serrod@gmail.com>
2020-03-04 01:32:15 -0500

    added input to change input and save file as tiker symbol

--- getTimeSeries.py
+++ getTimeSeries.py
@@ -2,11 +2,12 @@ import requests
 import json
 from privateKey import privateKeyModule
 
-# get results and save to .json file 
-jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPY&datatype=json&apikey='+privateKeyModule.keyValue
+print("provide TICKER Symbol to fetch")
+tickerInput = str(input()) 
+jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+tickerInput+'&datatype=json&apikey='+privateKeyModule.keyValue
 jsonResult = requests.get(jsonUrl).json()
 
-with open('file.json', 'w', encoding='utf-8') as jsonFile:
+with open(tickerInput+'.json', 'w', encoding='utf-8') as jsonFile:
     json.dump(jsonResult, jsonFile, ensure_ascii=False, indent=4)
 
 
