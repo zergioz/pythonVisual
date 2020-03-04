a6697e13947b989ccdbf792a9fbd39f3fa0a8f6c
Sergio F. Rodriguez <serrod@gmail.com>
2020-03-04 15:52:02 -0500

    extracting filepath as import module

--- getTimeSeries.py
+++ getTimeSeries.py
@@ -4,8 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from privateKey import privateKeyModule
-
-filePath = 'C:/Users/Sergio F. Rodriguez/Documents/pythonVisual/'
+from filePath import filepathModule
 
 def getJson(ticker):
     jsonUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&datatype=json&apikey='+privateKeyModule.keyValue
@@ -20,8 +19,8 @@ def getInput():
     return tickerInput
 
 def convertToCsv(ticker):
-    jsonFile = pd.read_json (filePath+ticker+'.json')
-    export_csv = jsonFile.to_csv (filePath+ticker+'.csv', index = None, header=True)
+    jsonFile = pd.read_json (filepathModule.filePath+ticker+'.json')
+    export_csv = jsonFile.to_csv (filepathModule.filePath+ticker+'.csv', index = None, header=True)
     print("Finalized CSV convetion")
 
 tickerInput = getInput()
